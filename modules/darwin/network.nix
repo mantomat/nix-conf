a4bf186a8b25118b5060f765191a73d711cd59f5
Matteo Mantoan <mattemant@gmail.com>
2025-07-21 12:14:30 +0200

    fix hostname

--- modules/darwin/network.nix
+++ modules/darwin/network.nix
@@ -1,6 +1,6 @@
 { ... }: {
-  networking.computerName = "mntmtt-mba";
-  networking.hostName = "mntmtt-mba";
+  networking.computerName = "mantomat";
+  networking.hostName = "mantomat";
   networking.knownNetworkServices = [ "AX88179A" "Thunderbolt Bridge" "Wi-Fi" ];
   networking.dns = [ "8.8.8.8" "8.8.4.4" ];
 }
